1b41b5891299ea85daa2c183e16ed10f75bcbc40
Eakapap Mingsrisuk <ddntri519@gmail.com>
2026-02-11 16:00:44 +0700

    Comment providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,23 +5,23 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from 'sonner';
 
 // Dynamically import ClientMsalProvider to avoid SSR issues with msal
-const ClientMsalProvider = dynamic(() => import('@/components/ClientMsalProvider'), { 
+const ClientMsalProvider = dynamic(() => import('@/components/ClientMsalProvider'), {
   ssr: false,
   // loading: () => <div>Loading authentication...</div>
 });
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClientMsalProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <Toaster position="top-center" />
-        {children}
-      </ThemeProvider>
-    </ClientMsalProvider>
+    // <ClientMsalProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Toaster position="top-center" />
+      {children}
+    </ThemeProvider>
+    // </ClientMsalProvider>
   );
 }
